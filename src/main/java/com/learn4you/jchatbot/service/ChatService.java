303a3096c333a510c5f99acfad58c860f1754a20
Tien LE <letien.dev@gmail.com>
2026-03-28 02:33:00 +0100

    LAVIE-119: removed unusing imports

--- src/main/java/com/learn4you/jchatbot/service/ChatService.java
+++ src/main/java/com/learn4you/jchatbot/service/ChatService.java
@@ -1,16 +1,7 @@
 package com.learn4you.jchatbot.service;
 
-//import com.google.genai.Client;
-//import com.google.genai.types.GenerateContentConfig;
-//import com.google.genai.types.GenerateContentResponse;
 import com.learn4you.jchatbot.dto.request.ChatRequest;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.prompt.Prompt;
-//import org.springframework.ai.google.genai.GoogleGenAiChatModel;
-//import org.springframework.ai.google.genai.GoogleGenAiChatModel.ChatModel;
-//import org.springframework.ai.google.genai.GoogleGenAiChatOptions;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
