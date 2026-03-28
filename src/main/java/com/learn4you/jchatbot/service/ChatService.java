1746e7e0aea4c4917fa5b1ccec65ac9e28cf0c88
Tien LE <letien.dev@gmail.com>
2026-03-28 02:42:11 +0100

    LAVIE-119: Added Prompt with SystemMessage and UserMessage

--- src/main/java/com/learn4you/jchatbot/service/ChatService.java
+++ src/main/java/com/learn4you/jchatbot/service/ChatService.java
@@ -2,6 +2,9 @@ package com.learn4you.jchatbot.service;
 
 import com.learn4you.jchatbot.dto.request.ChatRequest;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,11 +18,24 @@ public class ChatService {
     public String chat(ChatRequest request) {
         System.out.println("input message: " + request.message());
 
+        SystemMessage systemMessage = new SystemMessage("""
+                You are an expert assistant about AI Engineer, and your name is LAVIE.
+                Your response should always be a formal voice.
+                """);
+        UserMessage userMessage = new UserMessage(request.message());
+        Prompt prompt = new Prompt(systemMessage, userMessage);
+
 //        return chatClient.prompt(request.message()).call().content();
+
         // https://docs.spring.io/spring-ai/reference/api/chatclient.html
+//        return this.chatClient
+//                       .prompt()
+//                       .user(request.message())
+//                       .call()
+//                       .content();
+
         return this.chatClient
-                       .prompt()
-                       .user(request.message())
+                       .prompt(prompt)
                        .call()
                        .content();
     }
